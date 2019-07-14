8156fe3b3b71f87bd82a9a361696e43054290476
Aníbal Henrique <annibalhsouza@gmail.com>
2019-07-14 14:15:44 -0300

    #ISSUE_14 Creating login method for authenticated users

--- .docker/api/routes/api.php
+++ .docker/api/routes/api.php
@@ -13,26 +13,31 @@ use Illuminate\Support\Facades\Validator;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::post('/register', function(Request $request) {
+Route::post('/login', function(Request $request) {
   $data = $request->all();
 
   $validacao = Validator::make($data, [
-        'name' => ['required', 'string', 'max:255'],
-        'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-        'password' => ['required', 'string', 'min:8', 'confirmed'],
+        'email' => 'required|string|email|max:255',
+        'password' => 'required|string',
   ]);
 
   if($validacao->fails()){
   	return $validacao->errors();
   }
 
-  $user = User::create([
-         'name' => $data['name'],
-         'email' => $data['email'],
-         'password' => Hash::make($data['password']),
-  ]);
+  if(Auth::attempt(['email'=>$data['email'], 'password'=>$data['password']])) {
+  	$user = auth()->user();
+  	$user->token = $user->createToken($user->email)->accessToken;
+  } else {
+  	return ['status'=>'Não foi possível autenticar usuário'];
+  }
 
-  $user->token = $user->createToken($user->email)->accessToken;
+  // $user = User::create([
+  //        'name' => $data['name'],
+  //        'email' => $data['email'],
+  //        'password' => Hash::make($data['password']),
+  // ]);
+
 
   return $user;
 
--- up.sh
+++ up.sh
@@ -5,6 +5,8 @@ composer update --no-plugins --no-scripts
 php artisan key:generate
 php artisan config:cache
 composer require laravel/passport
+composer require lucascudo/laravel-pt-br-localization
+php artisan vendor:publish --tag=laravel-pt-br-localization
 php artisan migrate 
 php artisan passport:install 
 php artisan serve --host=0.0.0.0
