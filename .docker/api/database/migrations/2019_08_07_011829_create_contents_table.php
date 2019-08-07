311b870aa07059819d28c101a11aed96ac4871db
Aníbal Henrique <annibalhsouza@gmail.com>
2019-08-06 23:24:37 -0300

    #ISSUE_77 Adding table of contents relationship with users and assigning fields to be used

---
+++ .docker/api/database/migrations/2019_08_07_011829_create_contents_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contents', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->string('title');
+            $table->longText('text');
+            $table->string('image');
+            $table->string('link');
+            $table->dateTime('link');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contents');
+    }
+}
