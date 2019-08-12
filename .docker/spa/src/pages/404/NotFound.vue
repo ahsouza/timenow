83a355c8f27ae820957f109e2c9ced29fa776ef1
Aníbal Henrique <annibalhsouza@gmail.com>
2019-08-12 01:40:32 -0300

    #ISSUE_97 Adding page 404

---
+++ .docker/spa/src/pages/404/NotFound.vue
@@ -0,0 +1,26 @@
+<template>
+
+<h1>404</h1>
+<h6>Page Not Found!</h6>
+  
+</template>
+
+<script>
+import GridVue from '@/components/layouts/GridVue'
+
+export default {
+  name: '404',
+  data() {
+    return {
+      
+    }
+  },
+  components:{
+    GridVue
+  }
+  
+}
+</script>
+<style scoped>
+
+</style>
