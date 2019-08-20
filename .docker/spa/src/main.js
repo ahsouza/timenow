cd072f6c9ab7804c66835b88e26a89bf098a5daf
Aníbal Henrique <annibalhsouza@gmail.com>
2019-08-19 21:55:48 -0300

    #ISSUE_122 Adding and preparing state management standard with vuex

--- .docker/spa/src/main.js
+++ .docker/spa/src/main.js
@@ -1,19 +1,19 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import Vuesax from 'vuesax'
-import Vuetify from 'vuetify'
 import axios from 'axios'
 import App from './App'
 import router from './router'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import 'vuesax/dist/vuesax.css' //Vuesax styles
+import 'vuesax/dist/vuesax.css'
 import 'material-icons/iconfont/material-icons.css'
 
 library.add(faUserSecret)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
-Vue.use(Vuetify)
+Vue.use(Vuex)
 Vue.use(Vuesax, {
   theme:{
     colors:{
@@ -29,7 +29,6 @@ Vue.use(Vuesax, {
       dark_gray:'#424242',
       dark_orange:'#E65100',
       dark_yellown:'#FFEA00',
-      // dark_pink:'#AD1457',
       dark_purple:'#4A148C',
       dark_red:'#AD1457',
       light_blue:'#81D4FA',
@@ -38,7 +37,6 @@ Vue.use(Vuesax, {
       light_gray:'#E0E0E0',
       light_orange:'#FFAB40',
       light_yellown:'#FFFF8D',
-      // light_pink:'#FF80AB',
       light_purple:'#AA00FF',
       light_red:'#E91E63'
     }
@@ -49,8 +47,21 @@ Vue.config.productionTip = false
 Vue.prototype.$http = axios
 Vue.prototype.$url = 'http://127.0.0.1:8000/api/'
 
+const store = {
+  state: {
+
+  },
+  getters: {
+
+  },
+  mutations: {
+
+  }
+}
+
 new Vue({
   el: '#app',
+  store: new Vuex.Store(store),
   router,
   components: { App },
   template: '<App/>'
