a64395d686035d8dc8f8eb5f96bb5c5ecb087d8f
Aníbal Henrique <annibalhsouza@gmail.com>
2019-08-28 22:25:34 -0300

    #ISSUE_159 Inserting Scroll page directive

--- .docker/spa/src/main.js
+++ .docker/spa/src/main.js
@@ -81,6 +81,19 @@ const store = {
   }
 }
 
+
+Vue.directive('scroll', {
+  inserted: function (el, binding) {
+    let f = function (evt) {
+      if (binding.value(evt, el)) {
+        window.removeEventListener('scroll', f)
+      }
+    }
+    window.addEventListener('scroll', f)
+  }
+})
+
+
 new Vue({
   el: '#app',
   store: new Vuex.Store(store),
